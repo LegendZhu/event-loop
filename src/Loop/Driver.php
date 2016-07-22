225d43b8ffd0090d48b70b4d900e5dd057b470b8
Andrew Carter <andrewcarter1992@gmail.com>
2016-07-22 10:37:56 +0100

    Update Driver.php

--- src/Loop/Driver.php
+++ src/Loop/Driver.php
@@ -175,31 +175,6 @@ interface Driver
     public function unreference($watcherId);
 
     /**
-     * Stores information in the loop bound registry. This can be used to store loop bound information. Stored
-     * information is package private. Packages MUST NOT retrieve the stored state of other packages.
-     *
-     * Therefore packages SHOULD use the following prefix to keys: `vendor.package.`
-     *
-     * @param string $key namespaced storage key
-     * @param mixed $value the value to be stored
-     *
-     * @return void
-     */
-    public function storeState($key, $value);
-
-    /**
-     * Fetches information stored bound to the loop. Stored information is package private. Packages MUST NOT retrieve
-     * the stored state of other packages.
-     *
-     * Therefore packages SHOULD use the following prefix to keys: `vendor.package.`
-     *
-     * @param string $key Namespaced storage key.
-     *
-     * @return mixed previously stored value or null if it doesn't exist
-     */
-    public function fetchState($key);
-
-    /**
      * Set a callback to be executed when an error occurs.
      *
      * Subsequent calls to this method will overwrite the previous handler.
